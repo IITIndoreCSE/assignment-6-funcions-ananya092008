0c5c1a199524e5188d03ea7ec848e3984e4547da
me250003013 <me250003013@iiti.ac.in>
2025-09-04 10:11:10 +0530

    Update prime_palindrome.cpp

--- prime_palindrome.cpp
+++ prime_palindrome.cpp
@@ -2,17 +2,23 @@
 using namespace std;
 
 bool isPrime(int n) {
-    // TODO: complete the function as per instructions
-
+    if (n <= 1) return false;
+    for (int i =2; i * <= n; i++){
+        if (n % i == 0) return false;
+    }
+    return true;
 }
 
 bool isPalindrome(int n) {
-    // TODO: complete the function as per instructions
-
+  int original = n, reversed = 0  ;
+    while (n > 0) {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10}
+    return original == reversed;
 }
 
 bool primePalindrome(int n) {
-    // TODO: complete the function as per instructions
+    return isprime(n) && ispalindrome(n);
     
 }
 
